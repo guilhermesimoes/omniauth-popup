bfd5eb84687d30c2411f79243835ebb75e7eff16
Guilherme Simoes <guilherme.rdems@gmail.com>
2013-08-04 01:47:54 +0100

    Supply the `name` option in the google_oauth2 config.
    Logging in with Google now uses a much better url.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,14 +4,14 @@ class SessionsController < ApplicationController
     omniauth = env['omniauth.auth']
     user = User.find_or_create_with_omniauth(omniauth)
     session[:user_id] = user.id
-    flash[:notice] = t('controllers.sessions.create.notice', provider: provider_name(omniauth.provider))
+    flash[:notice] = t('controllers.sessions.create.notice', provider: omniauth.provider.titleize)
     popup = env['omniauth.params']['popup']
     next_page = session.delete(:origin) || env['omniauth.origin']
     render_or_redirect(next_page, popup)
   end
 
   def failure
-    flash[:alert] = t('controllers.sessions.failure.alert', provider: provider_name(params[:strategy]))
+    flash[:alert] = t('controllers.sessions.failure.alert', provider: params[:strategy].titleize)
     # We should be using env['omniauth.params']['popup'] but Omniauth fails
     # on failure so we have to add the on_failure hook in the omniauth.rb file
     popup = params[:popup]
@@ -32,14 +32,6 @@ class SessionsController < ApplicationController
       render 'callback', layout: false
     else
       redirect_to page
-    end
-  end
-
-  def provider_name(provider)
-    if provider == 'google_oauth2'
-      'Google'
-    else
-      provider.titleize
     end
   end
 end
