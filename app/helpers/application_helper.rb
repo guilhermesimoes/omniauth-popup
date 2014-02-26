2a37cf2d2603da6ba70d476e569a8b1bbbfe15da
Guilherme Simoes <guilherme.rdems@gmail.com>
2014-02-26 00:54:38 +0000

    Improve link_to_login_with helper method.
    It now behaves a little more like the regular link_to method.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,30 @@
 module ApplicationHelper
-  def link_to_login_with(provider, url, popup = {})
-    link_to t('.login_with_link', provider: provider),
-      url,
-      class: 'js-popup',
-      data: { width: popup[:width], height: popup[:height] }
+  def link_to_login_with(provider, url, html_options = {})
+    add_default_class(html_options)
+    convert_popup_attributes(html_options)
+
+    link_to t('.login_with_link', provider: provider), url, html_options
+  end
+
+  private
+
+  def add_default_class(html_options)
+    default_class = "js-popup"
+
+    if html_options.has_key?(:class)
+      html_options[:class] << " " << default_class
+    else
+      html_options[:class] = default_class
+    end
+  end
+
+  def convert_popup_attributes(html_options)
+    width = html_options.delete(:width)
+    height = html_options.delete(:height)
+
+    if width && height
+      html_options[:data] ||= {}
+      html_options[:data].merge!({width: width, height: height})
+    end
   end
 end
