f084ca1e64a4c68bc913e08db42f8ccbc9f84b15
Guilherme Simoes <guilherme.rdems@gmail.com>
2014-04-13 18:43:07 +0100

    Refactor User method that creates a user with omniauth data.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,23 +1,18 @@
 class User < ActiveRecord::Base
   def self.find_or_create_with_omniauth(auth)
-    user = User.find_by(:provider => auth.provider, :uid => auth.uid)
-    if user
-      user.update_info(auth)
-    else
-      create_with_omniauth(auth)
-    end
+    user = find_by(auth.slice(:provider, :uid)) || initialize_from_omniauth(auth)
+    user.update_dynamic_attributes(auth)
   end
 
-  def self.create_with_omniauth(auth)
-    create! do |user|
+  def self.initialize_from_omniauth(auth)
+    new do |user|
       user.provider = auth.provider
       user.uid = auth.uid
       user.name = auth.info.name
-      user.image = auth.info.image
     end
   end
 
-  def update_info(auth)
+  def update_dynamic_attributes(auth)
     self.image = auth.info.image if auth.info.image?
     self.save!
     self
